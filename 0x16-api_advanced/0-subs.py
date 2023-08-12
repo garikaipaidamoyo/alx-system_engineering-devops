275e29d0dd1a6e58289aa1477fe3ab153a7d67e8
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-08-12 16:09:33 +0000

    Add changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+
 def number_of_subscribers(subreddit):
     import requests
-    sub_number = requests.get("https://www.reddit.com/r/{}/about.json"
-                             .format(subreddit),
-                             headers={"User-Agent": "My-User-Agent"},
-                             allow_redirects=False)
+    sub_number = requests.get(
+        "https://www.reddit.com/r/{}/about.json"
+        .format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+        )
     if sub_number.status_code >= 300:
         return 0
     return sub_number.json().get("data").get("subscribers")
