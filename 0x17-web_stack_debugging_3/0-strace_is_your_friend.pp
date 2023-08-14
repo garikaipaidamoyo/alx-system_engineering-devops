120a0943a7de2cf4102f89990a2e0e3dc0aa7eed
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-08-14 20:19:09 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,12 @@
- a wordpress site running on apache2
+# Fixing Apache returning a 500 error
+
+file { '/var/www/html/wp-settings.php':
+  ensure => present,  # Or whatever appropriate ensure value
+  # Add other relevant attributes here
+  }
+
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php && service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => File['/var/www/html/wp-settings.php'],  # Assuming this file resource exists
 }
