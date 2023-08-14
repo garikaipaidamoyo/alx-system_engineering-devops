86beadfa974be8b878d0a19115cd2300a3a795b5
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-08-14 19:24:48 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,16 +1,5 @@
-class apache_config_fix {
-  file { '/etc/apache2/sites-available/my-site.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/alx-system_engineering-devops/0x17-web_stack_debugging_3/my-site.conf',  
-    notify  => Service['apache2'],
-  }
-  
-  service { 'apache2':
-    ensure => running,
-    enable => true,
-  }
-  
-  notify { "File path: ${modulepath('alx-system_engineering-devops/0x17-web_stack_debugging_3')}": }
+ a wordpress site running on apache2
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
-
-include apache_config_fix
