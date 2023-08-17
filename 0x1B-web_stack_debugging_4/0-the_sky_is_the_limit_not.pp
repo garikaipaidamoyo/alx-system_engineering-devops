dbad0647dbe7db0484fb6eae5271378cbe1b0ac6
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-08-17 07:26:04 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
-# run_apache_bench.pp
-
+# This Puppet manifest executes Apache Bench to perform load testing on a URL.
 # exec { 'run_apache_bench':
 #   command => '/usr/bin/ab -n 2000 -c 100 http://172.17.0.3/',
 #   }   
