ae3072dab830f414bed1717e6f79d4c61cbf24c0
Paidamoyo Garikai <garikaipaidamoyo@gmail.com>
2023-08-17 07:16:28 +0000

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-#0-the_sky_is_the_limit_not.pp
+# run_apache_bench.pp
 
-exec { 'run_apache_bench':
-  command => '/usr/bin/ab -n 2000 -c 100 http://172.17.0.3/',
-}
+# exec { 'run_apache_bench':
+#   command => '/usr/bin/ab -n 2000 -c 100 http://172.17.0.3/',
+#   }   
